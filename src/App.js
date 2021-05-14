2efc2951b06130e2a94139acdd24a1f6a8dabe62
guro <guramidgebuadze@gmail.com>
2021-05-14 14:04:25 +0400

    created file structure and added context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import MainMenu from './Components/MainMenu';
+import Quiz from './Components/EdnScreen';
+import EndScreen from './Components/EdnScreen';
 import './App.css';
 
 function App() {
+  const [gameState, setGameState] = useState('menu');
   return (
     <div className='App'>
       <h1>Quiz App</h1>
+      {gameState === 'menu' && <MainMenu />}
+      {gameState === 'quiz' && <Quiz />}
+      {gameState === 'endScreen' && <EndScreen />}
     </div>
   );
 }
--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Quiz = () => {
-  return <div></div>;
+  return <div>Quiz</div>;
 };
 
 export default Quiz;
